9189128a985d4d312a28ee663d97565b8e80849c
tonniegeetz <tonniegeetz@gmail.com>
2024-07-01 23:04:55 +0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 # Add a console message so we can see output when the seed file runs
+
+puts "Seeding games..."
+
+Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+
+puts "Done seeding!"
